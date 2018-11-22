973a23017e6c2c5907ac173c719354ce4abc1642
05050084 <05050084@me.mcu.edu.tw>
2018-11-22 16:05:57 +0800

    2018/11/22

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -25,6 +25,8 @@ import Adafruit_DHT
 
 import time
 
+
+
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
@@ -52,6 +54,7 @@ while True:
     humidity, temperature =Adafruit_DHT.read_retry(sensor,pin)
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+
     else:
         print('Failed to get reading. Try again!')
         sys.exit(1)
